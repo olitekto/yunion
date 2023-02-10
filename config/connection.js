db0070a616a6b9beab13731d61d4788d4644de3d
obatienan <obatienan@weblogy.com>
2023-02-10 00:11:30 +0000

    fix db connexion

--- config/connection.js
+++ config/connection.js
@@ -3,20 +3,21 @@
 var mysql = require('mysql');
 
 // DB connexion
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
+    connectionLimit : 10,
     host     : 'sql.freedb.tech',
     user     : 'freedb_olivier',
     password : 'HWUe7k&#WdwPMM@',
     database : 'freedb_yunion_db'
   });
   
-  connection.connect(function(err) {
+  connection.getConnection(function(err) {
       if (err) {
         console.error('error connecting: ' + err.stack);
         return;
       }
     
-      console.log('connected as id ' + connection.threadId);
+      //console.log('connected as id ' + connection.threadId);
   });
 
   module.exports = connection;
